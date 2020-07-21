b20285a2a523598368d1d06b3c360fbc8a6359e1
Max Pereira <max@pereira.at>
2020-07-21 15:10:29 -0700

    Added episode air date to output

--- rtp.py
+++ rtp.py
@@ -5,11 +5,16 @@ from bs4 import BeautifulSoup
 
 startIndex = int(sys.argv[1])
 endIndex = int(sys.argv[2])
+outFile = sys.argv[3]
 
 while startIndex <= endIndex:
 	# Set up BS4
 	req = requests.get('https://www.rtp.pt/play/p'+str(startIndex)+'/')
 	bs = BeautifulSoup(req.text, 'html.parser')
+
+	# File output for debugging
+	with open("debug", "a") as deb:
+		deb.write(req.text)
 
 	vodString = str(startIndex)+": "
 
@@ -19,7 +24,7 @@ while startIndex <= endIndex:
 		vodString = vodString + "Invalid VOD."
 		print(vodString)
 
-		with open("output"+sys.argv[1]+".txt", "a") as outp:
+		with open(outFile, "a") as outp:
 			outp.write(vodString+'\n')
 
 		startIndex = startIndex+1
@@ -39,16 +44,22 @@ while startIndex <= endIndex:
 			program_title = bs.find('h1')
 			vodString = vodString + program_title.contents[0].strip() + " - "
 
+		# Get content air date
+		date_check = req.text.find('content_date: ')
+		if (date_check != -1):
+			air_date = req.text[date_check+15:date_check+25]
+			vodString = vodString + air_date + " - "
+
 		# Look for caption link in page source
 		if (req.text.find('vtt: ') != -1):
 			# Has captions
-			vodString = vodString + "CC Available."
+			vodString = vodString + "CC Available"
 		else:
 			# Does not have captions
-			vodString = vodString + "NO Captions."
+			vodString = vodString + "NO Captions"
 
 		print(vodString)
-		with open("output"+sys.argv[1]+".txt", "a") as outp:
+		with open(outFile, "a") as outp:
 			outp.write(vodString+'\n')
 
 		startIndex = startIndex+1
